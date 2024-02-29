3fbe769c0d4be438c528b443b619fed56f24743e
Shakya Tilak <shakyatilak45@gmail.com>
2024-02-29 12:24:58 +0530

    Initial Commit

---
+++ MCtest/MCtestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MCtestApp.swift
+//  MCtest
+//
+//  Created by Tilak Shakya on 29/02/24.
+//
+
+import SwiftUI
+
+@main
+struct MCtestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
